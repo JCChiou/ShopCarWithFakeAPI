a182303969c79ba539b3260582aa4be3a3db3658
JCChiou <rex2763@gmail.com>
2025-03-19 16:19:06 +0800

    使用dotenv處理機敏資訊

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:fake_shopcar/views/ProductListview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(ProviderScope(child: MaterialApp(home: ProductListView())));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
+  runApp(ProviderScope(child: MaterialApp(home: ProductListView())));
 }
